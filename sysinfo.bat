c5ae0ebc618dfe38814d848d07487ec2ad2e59a0
aditya dand <33375699+adityadand@users.noreply.github.com>
2019-03-06 23:36:41 +0530

    Add files via upload

---
+++ sysinfo.bat
@@ -0,0 +1,11 @@
+md "collected system information"
+cd "collected system information"
+SYSTEMINFO > systeminfo.txt
+ipconfig > "ip and network.txt"
+dir > directorycontent.txt
+assoc > fileassociated.txt
+driverquery > driverinfo.txt
+netstat > netstatus.txt
+tasklist > tasklists.txt
+cd / & tree > listviewofdata.txt
+nslookup > nslookup.txt
